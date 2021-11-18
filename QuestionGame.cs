0312e434ecf4dd16ef7010c00a80cbae1211fa0e
vvahla <vahlaville@gmail.com>
2021-11-18 16:59:38 +0200

    Update QuestionGame.cs

--- QuestionGame.cs
+++ QuestionGame.cs
@@ -6,7 +6,6 @@ namespace TrueOrFalse
   {
 		static void Main(string[] args)
     {
-      // Do not edit these lines
       Console.WriteLine("Welcome to 'True or False?'\nPress Enter to begin:");
       string entry = Console.ReadLine();
       Tools.SetUpInputStream(entry);
